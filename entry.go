a114fe86140d0e1b5efe706ab92666c76730f2e6
kiquetal <kiquetal@gmail.com>
2024-10-20 00:00:25 -0300

    Adding the conf to talk with postgres

--- entry.go
+++ entry.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/kiquetal/boot-web-crawler/internal/database"
+	_ "github.com/lib/pq"
 	"os"
 )
 
 func main() {
 	// check if the executable have received 1 parameter
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("error opening db")
+		os.Exit(1)
+	}
+	fmt.Printf("db: %v\n", db)
+	fmt.Printf("DB URL: %v\n", dbURL)
+	defer db.Close()
+
+	_ = database.New(db)
+
+	// check if the executable have received 1 parameter
+
 	if len(os.Args) < 2 {
 		// if not, exit with status code 1
 		fmt.Println("no website provided")
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require golang.org/x/net v0.29.0
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 )
